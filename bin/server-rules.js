41ce1010f081e95b2af9a43121396b5f859354a1
Victor Stone <victor.stone@gmail.com>
2015-10-31 20:28:08 -0400

    going public with rules

---
+++ bin/server-rules.js
@@ -0,0 +1,84 @@
+
+console.log('Importing Server Rules');
+
+var just_one = false;
+
+var validationTests = [
+
+  function(req,res) {
+    var catchCrawler = req.url.match(/offset=([0-9]+)/);
+    if( catchCrawler !== null ) {
+        if( Number(catchCrawler[1]) > 30000 ) {
+          res.statusCode = 500;
+          res.end();
+          return false;
+        }
+    }
+    return true;    
+  },
+
+  function(req,res) {
+    var userAgent = req.headers['user-agent'];
+    if( req.url.match(/^\/dig/) && userAgent.match(/Dalvik/) ) {
+      res.statusCode = 404;
+      res.end();
+      return false;
+    }
+    return true;    
+  }
+
+];
+
+function isValidRequest(req,res) {
+
+  for( var i = 0; i < validationTests.length; i++ ) {
+    if( !validationTests[i](req,res) ) {
+      return false;
+    }  
+  }
+  return true;
+}
+
+function skipThese(req,res) {
+  return req.url.match(/(.js|.css|.html|.png|.eot|.otf|.ttf|.woff|.woff2|woff2\?v\=4.4\.0|.svg|.jpg|.jpeg|.ico)$/) !== null;
+}
+
+
+_serverRules = function(ctx) {
+
+  var req = ctx.req;
+  var res = ctx.res;
+
+  if( skipThese(req,res) ) {
+    return true;
+  }
+
+  if( !isValidRequest(req,res) ) {
+    return false;
+  }
+
+  if( !just_one ) {
+    ctx.sysLog.write( { req: util.inspect(req) } );
+    just_one = true;
+  }
+
+  return true;
+};
+
+function dumpCleanReq(req) {
+    var r = {};
+    var d = {
+      socket: 1,
+      connection: 1,
+      _readableState: 1,
+      client: 1,
+      rawHeaders: 1
+    };
+    for( var k in req ) {
+      if( typeof req[k] !== 'function' && !(k in d) ) {
+        r[k] = req[k];
+      }
+    }
+    console.log(r);  
+}
+
