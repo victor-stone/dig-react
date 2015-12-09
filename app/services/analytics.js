3bb83fd8392092822f5cae601312f2229d761413
Victor Stone <victor.stone@gmail.com>
2015-12-08 21:32:37 -0800

    wrong type for ga event

--- app/services/analytics.js
+++ app/services/analytics.js
@@ -5,11 +5,11 @@ import AudioService from '../services/audio-player';
 module.exports = function() {
 
   AudioService.on( events.NOW_PLAYING, function(upload) {
-    window.ga('send', 'event', 'Audio', 'play', env.appName + '-generic', upload.id + '-' + upload.name );
+    window.ga('send', 'event', 'Audio', 'play', env.appName + '-generic', upload.id  );
   });
 
   env.on( events.DOWNLOAD, function(upload) {
-    window.ga('send', 'event', 'File', 'download', env.appName + '-generic', upload.id + '-' + upload.name );
+    window.ga('send', 'event', 'File', 'download', env.appName + '-generic', upload.id  );
   });
 
 };
