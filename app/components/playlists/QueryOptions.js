3e26331a1f5ba2879516cce9059eab28e99c5c0e
Victor Stone <victor.stone@gmail.com>
2016-01-08 21:07:06 -0800

    added reqtag type for dyn playlists

--- app/components/playlists/QueryOptions.js
+++ app/components/playlists/QueryOptions.js
@@ -4,9 +4,40 @@ import InstrumentalOnlyFilter  from '../InstrumentalOnlyFilter';
 import ArtistFilter            from '../ArtistFilter';
 import TagFilter               from '../TagFilter';
 import TagsExtra               from '../TagsExtra';
+import { QueryParamTracker }   from '../../mixins';
+import { TagString }           from '../../unicorns';
 import { LicenseFilter,
          SortFilter,
          OptionsWrap }    from '../QueryOptions';
+
+const TypeFilter = React.createClass({ 
+
+  mixins: [QueryParamTracker],
+
+  stateFromParams: function(queryParams) {
+    return { reqtags: new TagString( queryParams.reqtags ).filter(/^(remix|sample|acappella)$/) };
+  },
+
+  performQuery: function() {
+    var typetag = this.refs['typelist'].value;
+    var tags    = new TagString(this.props.store.model.queryParams.reqtags)
+                        .remove('remix,sample,acappella')
+                        .add(typetag);
+    this.props.store.applySoftParams( { reqtags: tags.toString() } );
+  },
+  
+  render: function() {
+    return (
+        <select ref="typelist" id="typelist" value={this.state.reqtags} onChange={this.performQuery} className="form-control" >
+          <option value="remix">{"remixes"}</option>
+          <option value="sample">{"samples"}</option>
+          <option value="acappella">{"a cappellas"}</option>
+        </select>
+      );
+  }
+
+});
+
 
 function PlaylistQueryOptions(props) {
 
@@ -14,6 +45,9 @@ function PlaylistQueryOptions(props) {
 
     return ( 
       <OptionsWrap>
+        <li>
+          <TypeFilter store={store} />
+        </li>
         <li>
           <LicenseFilter store={store} />
         </li>
