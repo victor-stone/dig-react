44c510a73f639a3cf27d5090be8add5b16c144c5
Victor Stone <victor.stone@gmail.com>
2016-06-27 16:21:08 -0700

    casing in file (1)

--- app/models/ccmixter/feed.js
+++ app/models/ccmixter/feed.js
@@ -1,4 +1,4 @@
-import Model from '../Model';
+import Model from '../model';
 import {UserFeedObjTypes} from '../user-feed-types';
 
 /*
--- app/models/ccmixter/tags.js
+++ app/models/ccmixter/tags.js
@@ -1,4 +1,4 @@
-import Model from '../Model';
+import Model from '../model';
 
 class Tag extends Model {
   constructor() {  
