0fad6106e28ca171c6a407642098db110cd8aa3d
Michael Gregoire <mgregoire@nvzion.com>
2018-05-25 10:00:39 -0400

    Pointed onPlay to the correct items location so that AudioService.playlist isn't set to null

--- app/components/playlists/EditableTrackList.js
+++ app/components/playlists/EditableTrackList.js
@@ -1,8 +1,8 @@
-import React                  from 'react';
+import React from 'react';
 import BoundEditableTrackList from '../bound/EditableTrackList';
-import { bindAll }            from '../../unicorns';
-import AudioService           from '../../services/audio-player';
-import LinkToPlaylist         from '../services/LinkToPlaylistRoute';
+import { bindAll } from '../../unicorns';
+import AudioService from '../../services/audio-player';
+import LinkToPlaylist from '../services/LinkToPlaylistRoute';
 
 /*
   EditTrackList wrapper that displays/edits the 'tracks'
@@ -11,46 +11,56 @@ import LinkToPlaylist         from '../services/LinkToPlaylistRoute';
   props
     store - from stores/playlist
 */
-class EditableTrackList extends React.Component
-{
+class EditableTrackList extends React.Component {
   constructor() {
     super(...arguments);
-    bindAll( this, 'onPlay', 'onTrackDelete', 'onSort' );
+    bindAll(this, 'onPlay', 'onTrackDelete', 'onSort');
   }
 
   onPlay() {
-    const { items,head:{id} } = this.props.store.model;
+    const {
+      tracks,
+      head: { id }
+    } = this.props.store.model;
 
-    AudioService.playlistURL = LinkToPlaylist.url({id});
-    AudioService.playlist = items;
+    AudioService.playlistURL = LinkToPlaylist.url({ id });
+    AudioService.playlist = tracks.model.items;
   }
 
   onTrackDelete(model) {
     const { store } = this.props;
 
-    store.removeTrack(model.id).then( () => store.model.tracks.paginate(0) );
+    store.removeTrack(model.id).then(() => store.model.tracks.paginate(0));
   }
 
   onSort(sortSpec) {
     const { store } = this.props;
 
-    store.reorder(sortSpec).then( () => store.model.tracks.paginate(0) );
+    store.reorder(sortSpec).then(() => store.model.tracks.paginate(0));
   }
 
   render() {
-    const { store, 
-            store: { permissions: {canEdit=false} = {} } } = this.props;
+    const {
+      store,
+      store: { permissions: { canEdit = false } = {} }
+    } = this.props;
 
-    const { model: { tracks,
-                     head:{isDynamic}} } = store;
+    const {
+      model: {
+        tracks,
+        head: { isDynamic }
+      }
+    } = store;
 
-    return  (<BoundEditableTrackList 
-                store={tracks} 
-                canEdit={canEdit && !isDynamic}
-                onDelete={this.onTrackDelete}
-                onPlay={this.onPlay}
-                onSort={this.onSort}
-             />);
+    return (
+      <BoundEditableTrackList
+        store={tracks}
+        canEdit={canEdit && !isDynamic}
+        onDelete={this.onTrackDelete}
+        onPlay={this.onPlay}
+        onSort={this.onSort}
+      />
+    );
   }
 }
 
